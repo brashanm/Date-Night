3b66662e7fe6c43c45730cc6247eda164c611d68
Brashan <brashanm@gmail.com>
2023-12-04 19:57:53 -0500

    Initial Commit

---
+++ Map/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Map
+//
+//  Created by Brashan Mohanakumar on 2023-12-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Map/MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapApp.swift
+//  Map
+//
+//  Created by Brashan Mohanakumar on 2023-12-04.
+//
+
+import SwiftUI
+
+@main
+struct MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
